e6614d7b3449e37faafd9caf7b4356da0dcbf20c
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-02-18 13:44:53 +0330

    feat: preparing country model

---
+++ Pestivirus/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿namespace Pestivirus.Models;
+
+public class Country
+{
+    public string? Title { get; set; }
+    public int Cases { get; set; }
+    public int TodayCases { get; set; }
+    public int Deaths { get; set; }
+    public int TodayDeaths { get; set; }
+    public int Recovered { get; set; }
+    public int Active { get; set; }
+    public int Critical { get; set; }
+    public string? FlagUri { get; set; }
+}
+
