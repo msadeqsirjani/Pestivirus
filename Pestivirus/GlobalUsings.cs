3dfe12cfce433ff71eca18b6c23a262d77c5ac08
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-02-18 13:47:12 +0330

    feat: GlobalUsings is available

---
+++ Pestivirus/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Pestivirus.Models;
+global using System.Collections.Generic;
+global using System.Threading.Tasks;
+global using Pestivirus.Dtos;
+global using System.Linq;
+global using System.Net.Http;
+global using System.Net.Http.Json;
+global using System.Text.Json;
