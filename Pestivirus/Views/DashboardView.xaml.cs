6d2cd93e19b1cafffea3fb49511f8d15e9833b63
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-02-18 14:32:47 +0330

    feat: define dashboard view

---
+++ Pestivirus/Views/DashboardView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pestivirus.Views;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class DashboardView
+{
+    public DashboardView()
+    {
+        InitializeComponent();
+    }
+}
