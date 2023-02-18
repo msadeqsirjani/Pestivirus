09c285a9051f7a97361d08f09507301d0ef72f5d
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2023-02-18 13:46:53 +0330

    feat: implement country statistics get top cases method using web services

---
+++ Pestivirus/Services/CountryStaticticsService.cs
@@ -0,0 +1,34 @@
+﻿namespace Pestivirus.Services;
+
+public class CountryStatisticsService : ICountryStatisticsService
+{
+    private const string _uri = "https://corona.lmao.ninja/v3/covid-19/countries?sort=cases";
+
+    public async Task<IEnumerable<Country>> GetTopCases()
+    {
+        using var client = new HttpClient();
+
+        var response = await client.GetAsync(_uri);
+
+        var result = await response.Content.ReadFromJsonAsync<IEnumerable<CountryDto>>(new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        if (result == null)
+            return new List<Country>();
+
+        return result.Select(country => new Country
+        {
+            Title = country.Country,
+            Cases = country.Cases,
+            Deaths = country.Deaths,
+            TodayCases = country.TodayCases,
+            TodayDeaths = country.TodayDeaths,
+            Active = country.Active,
+            Critical = country.Critical,
+            Recovered = country.Recovered,
+            FlagUri = country.CountryInfo?.Flag
+        });
+    }
+}
